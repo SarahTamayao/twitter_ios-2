3747848252293ca543472742afe67d2d7dd63e84
Alex Long <alelong@ucdavis.edu>
2020-10-11 00:24:31 -0700

    Added time of posts

--- Twitter/TweetCellTableViewCell.swift
+++ Twitter/TweetCellTableViewCell.swift
@@ -14,6 +14,7 @@ class TweetCellTableViewCell: UITableViewCell {
     @IBOutlet weak var userNameLabel: UILabel!
     @IBOutlet weak var tweetContent: UILabel!
     
+    @IBOutlet weak var timeLabel: UILabel!
     
     
     override func awakeFromNib() {
